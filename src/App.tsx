50ec40db28332c6ca34d5883c34fef5794880587
Nqvinh7603 <nqvinh7603@gmail.com>
2024-12-30 13:19:48 +0700

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ConfigProvider } from "antd";
+import viVN from "antd/locale/vi_VN";
 import dayjs from "dayjs";
 import "dayjs/locale/vi";
 import isBetween from "dayjs/plugin/isBetween";
@@ -7,9 +9,8 @@ import isoWeek from "dayjs/plugin/isoWeek";
 import quarterOfYear from "dayjs/plugin/quarterOfYear";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
+import { Toaster } from "react-hot-toast";
 import AppRouter from "./router/AppRouter";
-import { ConfigProvider } from "antd";
-import viVN from "antd/locale/vi_VN";
 
 export const TIME_ZONE = "Asia/Ho_Chi_Minh";
 dayjs.locale("vi");
@@ -42,6 +43,24 @@ function App() {
         <ReactQueryDevtools initialIsOpen={false} />
         <AppRouter />
       </QueryClientProvider>
+      <Toaster
+        position="top-center"
+        containerStyle={{
+          marginTop: "0.25rem",
+        }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 3000,
+          },
+          style: {
+            fontSize: "1rem",
+            padding: "0.75rem 1rem",
+          },
+        }}
+      />
     </ConfigProvider>
   );
 }
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,18 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 const router = createBrowserRouter([
-  //   {
-  //     element: <Login />,
-  //     path: "/login",
-  //   },
-  //   {
-  //     element: (
-  //       <ProtectedRoute>
-  //         <AdminLayout />
-  //       </ProtectedRoute>
-  //     ),
-  //     errorElement: <ErrorIndicator />,
-  //   },
+  {
+    // element: (
+    //   <ProtectedRoute>
+    //     <AdminLayout />
+    //   </ProtectedRoute>
+    // ),
+    //errorElement: <ErrorIndicator />,
+  },
 ]);
 const AppRouter: React.FC = () => {
   return <RouterProvider router={router} />;
